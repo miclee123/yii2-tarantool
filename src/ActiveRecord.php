e94ea9b78fc0294a376914483f06610b4535eab4
Michael Petrov <miclee123@mail.ru>
2021-04-03 17:42:49 +0300

    create classes

---
+++ src/ActiveRecord.php
@@ -0,0 +1,83 @@
+<?php
+
+
+namespace miclee123\yii2tarantool;
+
+
+use miclee\yii2tarantool\ActiveQuery;
+use yii\base\InvalidConfigException;
+use yii\db\BaseActiveRecord;
+use Yii;
+
+/**
+ * ActiveRecord is the base class for classes representing relational data in terms of objects.
+ *
+ * This class implements the ActiveRecord pattern for the [tarantool](https://tarantool.io/) key-value store.
+ *
+ * For defining a record a subclass should at least implement the [[attributes()]] method to define
+ * attributes. A primary key can be defined via [[primaryKey()]] which defaults to `id` if not specified.
+ *
+ * The following is an example model called `Customer`:
+ *
+ * ```php
+ * class Customer extends \miclee123\yii2tarantool\ActiveRecord
+ * {
+ *     public function attributes()
+ *     {
+ *         return ['id', 'name', 'address', 'registration_date'];
+ *     }
+ * }
+ * ```
+ *
+ * @author Michael Petrov <miclee123@mail.ru>
+ * @since 2.0
+ */
+class ActiveRecord extends BaseActiveRecord
+{
+    /**
+     * Returns the database connection used by this AR class.
+     * By default, the "tarantool" application component is used as the database connection.
+     * You may override this method if you want to use a different database connection.
+     * @return Connection the database connection used by this AR class.
+     */
+    public static function getDb()
+    {
+        return Yii::$app->get('tarantool');
+    }
+    /**
+     * @inheritdoc
+     * @return ActiveQuery the newly created [[ActiveQuery]] instance.
+     */
+    public static function find()
+    {
+        return Yii::createObject(ActiveQuery::className(), [get_called_class()]);
+    }
+
+    /**
+     * Returns the primary key name(s) for this AR class.
+     * This method should be overridden by child classes to define the primary key.
+     *
+     * Note that an array should be returned even when it is a single primary key.
+     *
+     * @return string[] the primary keys of this record.
+     */
+    public static function primaryKey()
+    {
+        return ['id'];
+    }
+
+    /**
+     * Returns the list of all attribute names of the model.
+     * This method must be overridden by child classes to define available attributes.
+     * @return array list of attribute names.
+     */
+    public function attributes()
+    {
+        throw new InvalidConfigException('The attributes() method of redis ActiveRecord has to be implemented by child classes.');
+    }
+
+    public function insert($runValidation = true, $attributes = null)
+    {
+        // TODO: Implement insert() method.
+    }
+}
---
+++ src/Connection.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace miclee123\yii2tarantool;
+
+
+use yii\base\Component;
+
+class Connection extends Component
+{
+
+}
